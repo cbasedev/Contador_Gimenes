d1839d90122050c49922de55feb8652d07a673bb
Christian Gimenes <cgimenes@cbase.com.ar>
2022-09-06 19:41:11 -0300

    Clase 2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,53 @@
+Vue.component('mi-card', {
+    
+    props: {
+        product: Object
+    },
+
+    template: 
+        `<div>
+            <img :src="product.image" class="card-img-top" :alt="product.description">
+            <div class="card-body">
+            <p class="card-text">{{product.description}}</p>
+            <a href="#" class="btn btn-primary">Comprar <b>$ {{product.price}}</b></a>
+            </div>
+         </div>`
+
+})
+
 var app = new Vue({
     el: '#app',
     data:{
-        cont: 0
+        cont: 0,
+        foods: [
+            {
+                id: 1,
+                description: 'Fideos con Salsa fileto',
+                image: './assets/fideos.jpeg',
+                price: 800.00
+            },
+            {
+                id: 2,
+                description: 'Milanesas con papas fritas',
+                image: './assets/milanesa.jpeg',
+                price: 950.00
+            },
+            {
+                id: 3,
+                description: 'Bife con puré',
+                image: './assets/bife.jpeg',
+                price: 880.00
+            },
+            {
+                id: 4,
+                description: 'Tortilla de papa',
+                image: './assets/tortilla.png',
+                price: 430.00
+            },
+        ]
+
     },
+
     methods:{
         restoCont(){
             this.cont--;
